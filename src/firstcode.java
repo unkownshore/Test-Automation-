30acdeebbe87f00938847762d823fbe9715a94d8
abu shayed <ashayed560@gmail.com>
2022-03-16 00:09:20 -0400

    new comment

---
+++ src/firstcode.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner; 
+public class firstcode {
+
+	public static void main(String[] args) {
+		
+		// initializing scanner 
+		Scanner kb = new Scanner(System.in);
+		int n =kb.nextInt();
+		
+		switch(n) {
+		case 1:
+			System.out.println("Monday");
+		case 2:
+			System.out.println("Tuesday");
+		case 3:
+			System.out.println("Wednesday");
+		case 4:
+			System.out.println("Thursday");
+		case 5:
+			System.out.println("Friday");
+		case 6:
+			System.out.println("Saturday");
+		case 7:
+			System.out.println("Sunday");
+		default:
+			System.out.println("Holiday!");
+			
+		}
+
+	}
+
+}
